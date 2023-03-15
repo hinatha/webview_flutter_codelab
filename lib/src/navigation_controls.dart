a4848afafbf4e0da37a8e2096d3ce079aff8bcbe
hinatha <hinatha0110@gmail.com>
2023-03-15 14:50:34 +0900

    Add comments in navigation_controls.dart

--- lib/src/navigation_controls.dart
+++ lib/src/navigation_controls.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+// Just define NavigationControls
 class NavigationControls extends StatelessWidget {
+  // create a new instance of the NavigationControls widget.
+  // this.controller is WebViewController.
+  // super.key is calling the constructor's key of StatelessWidget.
+  // assign super.key to NavigationControls widget.
   const NavigationControls({required this.controller, super.key});
 
+  // Define controller
   final WebViewController controller;
 
+  // Create the page of navigation controls part.
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
+        // Go back to former page button
         IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
+          icon: const Icon(Icons.arrow_back_ios), // set button icon like "<".
           onPressed: () async {
             final messenger = ScaffoldMessenger.of(context);
+            // In case of going back
             if (await controller.canGoBack()) {
               await controller.goBack();
             } else {
+              // In case of not going back
+              // show the message of can't go back
               messenger.showSnackBar(
                 const SnackBar(content: Text('No back history item')),
               );
@@ -24,13 +35,18 @@ class NavigationControls extends StatelessWidget {
             }
           },
         ),
+        // Go to forward page button
         IconButton(
-          icon: const Icon(Icons.arrow_forward_ios),
+          icon:
+              const Icon(Icons.arrow_forward_ios), // set button icon like ">".
           onPressed: () async {
             final messenger = ScaffoldMessenger.of(context);
+            // In case of going forward
             if (await controller.canGoForward()) {
               await controller.goForward();
             } else {
+              // In case of not going forward
+              // show the message of can't go forward
               messenger.showSnackBar(
                 const SnackBar(content: Text('No forward history item')),
               );
@@ -38,10 +54,11 @@ class NavigationControls extends StatelessWidget {
             }
           },
         ),
+        // Reload the page button
         IconButton(
-          icon: const Icon(Icons.replay),
+          icon: const Icon(Icons.replay), // set the button icon
           onPressed: () {
-            controller.reload();
+            controller.reload(); // Execute reloading the page
           },
         ),
       ],
